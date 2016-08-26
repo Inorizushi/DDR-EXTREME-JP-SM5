8633453eb0ce33ce52e9349b6c4438e35511c505
Inorizushi <inorizushi@gmail.com>
2016-08-26 10:18:30 -0700

    Move Danger Animation to default.lua

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -1,9 +1,43 @@
-local t = Def.ActorFrame{
+local t = Def.ActorFrame{};
 
+t[#t+1] = Def.ActorFrame{
+	Def.ActorFrame{
+		Name="DangerP1";
+		Def.ActorFrame{
+			Name="Single";
+			BeginCommand=function(self)
+			local style = GAMESTATE:GetCurrentStyle()
+			local styleType = style:GetStyleType()
+			local bDoubles = (styleType == 'StyleType_OnePlayerTwoSides' or styleType == 'StyleType_TwoPlayersSharedSides')
+				self:visible(not bDoubles)
+			end;
+		HealthStateChangedMessageCommand=function(self, param)
+			if param.PlayerNumber == PLAYER_1 then
+				if param.HealthState == "HealthState_Danger" then
+					self:RunCommandsOnChildren(cmd(playcommand,"Show"))
+				else
+					self:RunCommandsOnChildren(cmd(playcommand,"Hide"))
+				end
+			end
+		end;
+			Def.Quad{
+				InitCommand=cmd(visible,false;FullScreen;diffuse,color("0,0,0,1"));
+				ShowCommand=cmd(visible,true;diffuseblink;effectperiod,1);
+				HideCommand=cmd(visible,false);
+			};
+			LoadActor("_danger")..{
+				InitCommand=cmd(visible,false;Center;zoomto,640,480);
+				ShowCommand=cmd(visible,true;diffuseshift;effectcolor1,color("0,0,0,1");effectcolor2,color("0,0,0,0")effectclock,"music";);
+				HideCommand=cmd(visible,false);
+			};
+		};
+	};
+};
 --t[#t+1] = LoadActor("frame")..{
 --	InitCommand=cmd(Center);
 --};
-
+};
+t[#t+1] = Def.ActorFrame{
 -- Extra lifemeter under, shit breaks because lol reverse.
 	Def.Quad{
 		InitCommand=cmd(CenterX;diffuse,color("0,0,0,1"));
@@ -26,8 +60,6 @@ local t = Def.ActorFrame{
 			end;
 		end;
 	};
-
-	LoadActor("Danger");
 };
 
 return t;
