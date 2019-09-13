f831bd668525dd28e1cf9b015c44b5673b7903c3
inorizushi <inorizushi@gmail.com>
2019-09-13 17:03:55 -0500

    Cleanup Graphics folder a bit, Re-work Select Difficulty even more.

--- Graphics/ScreenSelectDifficulty Icon/BEGINNER.lua
+++ Graphics/ScreenSelectDifficulty Icon/BEGINNER.lua
@@ -1,5 +1,9 @@
 return Def.ActorFrame {
-	LoadActor("bar")..{
+	Def.Quad{
+		InitCommand=function(self)
+			self:setsize(614,48)
+			self:MaskSource()
+		end,
 		OnCommand=cmd(cropbottom,1;sleep,0.264;sleep,0.033;cropleft,0.493;cropright,0.493;linear,0.264;cropbottom,0;linear,0.726;cropleft,0;cropright,0);
 		OffCommand=cmd(cropbottom,0;cropleft,0;cropright,0;sleep,0.726;linear,0.726;cropleft,0.493;cropright,0.493;linear,0.264;cropbottom,1);
 		SwitchToPage1Command=cmd(sleep,0.2;linear,0.5;cropleft,0;cropright,0);
--- Graphics/ScreenSelectDifficulty Icon/NONSTOP.lua
+++ Graphics/ScreenSelectDifficulty Icon/NONSTOP.lua
@@ -1,8 +1,12 @@
 return Def.ActorFrame {
-  LoadActor("bar")..{
+	Def.Quad{
+		InitCommand=function(self)
+			self:setsize(614,48)
+			self:MaskSource()
+		end,
 		OnCommand=cmd(cropleft,1;cropright,1);
 		OffCommand=cmd(cropbottom,0;cropleft,0;cropright,0;sleep,0.726;linear,0.726;cropleft,0.493;cropright,0.493;linear,0.264;cropbottom,1);
-    SwitchToPage1Command=cmd(sleep,0.2;linear,0.5;cropleft,1;cropright,1);
+		SwitchToPage1Command=cmd(sleep,0.2;linear,0.5;cropleft,1;cropright,1);
 		SwitchToPage2Command=cmd(sleep,0.2;linear,0.5;cropleft,0;cropright,0);
 	};
 };
