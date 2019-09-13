3b490dc7fc665558904519205d36ca2bd8027516
Jose_Varela <jo.lu6853@gmail.com>
2019-09-13 07:16:42 -0500

    Remake ModIcons to use lua
    goodbye elseifs

--- BGAnimations/ScreenSelectMusic decorations/modicons/default.lua
+++ BGAnimations/ScreenSelectMusic decorations/modicons/default.lua
@@ -6,6 +6,7 @@ local yPosPlayer = {
 }
 
 for _, pn in pairs(GAMESTATE:GetEnabledPlayers()) do
+	local spn = ToEnumShortString(pn)
 	t[#t+1] = LoadActor("mod bar")..{
 		Name = pn,
 		InitCommand=function(self)
@@ -16,44 +17,27 @@ for _, pn in pairs(GAMESTATE:GetEnabledPlayers()) do
 		OnCommand=cmd(addx,-SCREEN_WIDTH;sleep,0.317;decelerate,0.766;addx,SCREEN_WIDTH);
 		OffCommand=cmd(accelerate,0.783;addx,-640);
 	};
-end
 
-t[#t+1] = LoadActor("p1badge")..{
-	InitCommand=function(self)
-		self:x(SCREEN_CENTER_X-320):y(SCREEN_TOP+52):halign(0);
-	end;
-	BeginCommand=function(self,param)
-		if GAMESTATE:IsPlayerEnabled('PlayerNumber_P1') then
-			self:visible(true);
-		else
-			self:visible(false);
+	t[#t+1] = LoadActor( spn.."badge")..{
+		InitCommand=function(self)
+			self:x(SCREEN_CENTER_X-320)
+			:y( pn == PLAYER_1 and  SCREEN_TOP+52 or SCREEN_TOP+64)
+			:halign(0);
 		end;
-	end;
-	OnCommand=cmd(addx,-SCREEN_WIDTH;sleep,0.317;decelerate,0.766;addx,SCREEN_WIDTH);
-	OffCommand=cmd(accelerate,0.783;addx,-640);
-};
-
-t[#t+1] = LoadActor("p2badge")..{
-	InitCommand=function(self)
-		self:x(SCREEN_CENTER_X-320):y(SCREEN_TOP+64):halign(0);
-	end;
-	BeginCommand=function(self,param)
-		if GAMESTATE:IsPlayerEnabled('PlayerNumber_P2') then
-			self:visible(true);
-		else
-			self:visible(false);
+		BeginCommand=function(self,param)
+			self:visible( GAMESTATE:IsPlayerEnabled(pn) )
 		end;
-	end;
-	OnCommand=cmd(addx,-SCREEN_WIDTH;sleep,0.317;decelerate,0.766;addx,SCREEN_WIDTH);
-	OffCommand=cmd(accelerate,0.783;addx,-640);
-};
-
-t[#t+1] = Def.ActorFrame{
-	LoadActor( "Icons" ) .. {
-		InitCommand=cmd(player,PLAYER_1;CenterX;y,SCREEN_TOP+52.1;draworder,1);
 		OnCommand=cmd(addx,-SCREEN_WIDTH;sleep,0.317;decelerate,0.766;addx,SCREEN_WIDTH);
 		OffCommand=cmd(accelerate,0.783;addx,-640);
 	};
-};
+
+	t[#t+1] = Def.ActorFrame{
+		LoadActor( "Icons", pn ) .. {
+			InitCommand=cmd(player,pn;CenterX;y,SCREEN_TOP+52.1;draworder,1);
+			OnCommand=cmd(addx,-SCREEN_WIDTH;sleep,0.317;decelerate,0.766;addx,SCREEN_WIDTH);
+			OffCommand=cmd(accelerate,0.783;addx,-640);
+		};
+	};
+end
 
 return t;
