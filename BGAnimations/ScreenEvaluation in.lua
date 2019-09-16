a02f4fa5939c909dab30e2594772ad7af6271cce
Jose_Varela <josevarela@tutanota.com>
2019-09-16 17:23:14 -0500

    small Eval fix and ForLoop GamePlay in animation

--- BGAnimations/ScreenEvaluation in.lua
+++ BGAnimations/ScreenEvaluation in.lua
@@ -5,6 +5,6 @@ return Def.ActorFrame{
 	};
   Def.Quad{
     InitCommand=function(s) s:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y):stretchto(0,0,SCREEN_WIDTH,SCREEN_HEIGHT):diffuse( color("0,0,0,1") ) end,
-    OnCommand=function(s) s:diffusealpha,1;linear,0.266;diffusealpha,0) end
+    OnCommand=function(s) s:diffusealpha(1):linear(0.266):diffusealpha(0) end
   };
 };
--- BGAnimations/ScreenGameplay in/default.lua
+++ BGAnimations/ScreenGameplay in/default.lua
@@ -1,74 +1,53 @@
 local travelDist = SCREEN_WIDTH*1.7;
 
 local LeftToRight = Def.ActorFrame{
-	LoadActor("../LeftToRight_green");
+	Def.Sprite{ Texture="../LeftToRight_green" };
 	Def.Quad{
-		InitCommand=cmd(addx,64;diffuse,color("#000000FF");halign,0;zoomto,travelDist,32);
+		InitCommand=function(s)
+			s:addx(64):diffuse( color("#000000FF") ):halign(0):zoomto(travelDist,32)
+		end
 	};
 };
 
 local RightToLeft = Def.ActorFrame{
-	LoadActor("../RightToLeft_green");
+	Def.Sprite{ Texture="../RightToLeft_green" };
 	Def.Quad{
-		InitCommand=cmd(addx,-64;diffuse,color("#000000FF");halign,1;zoomto,travelDist,32);
+		InitCommand=function(s)
+			s:addx(-64):diffuse( color("#000000FF") ):halign(1):zoomto(travelDist,32)
+		end
 	};
 };
 
-return Def.ActorFrame{
-	Def.Quad{
-		InitCommand=cmd(Center;FullScreen;diffuse,color("0,0,0,1"));
-		OnCommand=cmd(linear,0.4;diffusealpha,0);
-	};
+local t = Def.ActorFrame{}
 
+t[#t+1] = Def.Quad{
+	InitCommand=function(s)
+		s:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y):stretchto(0,0,SCREEN_WIDTH,SCREEN_HEIGHT):diffuse( Color.Black )
+	end,
+	OnCommand=function(s)
+		s:linear(0.4):diffusealpha(0)
+	end;
+}
+
+for i=1,6 do
 	-- 6 left -> right
-	LeftToRight..{
-		InitCommand=cmd(x,SCREEN_LEFT-64;y,SCREEN_CENTER_Y-176);
-		OnCommand=cmd(linear,0.4;addx,SCREEN_WIDTH*1.7);
-	};
-	LeftToRight..{
-		InitCommand=cmd(x,SCREEN_LEFT-128;y,SCREEN_CENTER_Y-112);
-		OnCommand=cmd(linear,0.4;addx,SCREEN_WIDTH*1.7);
-	};
-	LeftToRight..{
-		InitCommand=cmd(x,SCREEN_LEFT-192;y,SCREEN_CENTER_Y-48);
-		OnCommand=cmd(linear,0.4;addx,SCREEN_WIDTH*1.7);
-	};
-	LeftToRight..{
-		InitCommand=cmd(x,SCREEN_LEFT-256;y,SCREEN_CENTER_Y+16);
-		OnCommand=cmd(linear,0.4;addx,SCREEN_WIDTH*1.7);
-	};
-	LeftToRight..{
-		InitCommand=cmd(x,SCREEN_LEFT-320;y,SCREEN_CENTER_Y+80);
-		OnCommand=cmd(linear,0.4;addx,SCREEN_WIDTH*1.7);
-	};
-	LeftToRight..{
-		InitCommand=cmd(x,SCREEN_LEFT-384;y,SCREEN_CENTER_Y+144);
-		OnCommand=cmd(linear,0.4;addx,SCREEN_WIDTH*1.7);
-	};
-
+	t[#t+1] = LeftToRight..{
+		InitCommand=function(s)
+			s:xy( SCREEN_LEFT-64*i,SCREEN_CENTER_Y-176+(64*(i-1)) )
+		end,
+		OnCommand=function(s)
+			s:linear(0.4):addx(SCREEN_WIDTH*1.7)
+		end
+	}
 	-- 6 right -> left
-	RightToLeft..{
-		InitCommand=cmd(x,SCREEN_RIGHT+64;y,SCREEN_CENTER_Y-144);
-		OnCommand=cmd(linear,0.4;addx,-SCREEN_WIDTH*1.7);
-	};
-	RightToLeft..{
-		InitCommand=cmd(x,SCREEN_RIGHT+128;y,SCREEN_CENTER_Y-80);
-		OnCommand=cmd(linear,0.4;addx,-SCREEN_WIDTH*1.7);
-	};
-	RightToLeft..{
-		InitCommand=cmd(x,SCREEN_RIGHT+192;y,SCREEN_CENTER_Y-16);
-		OnCommand=cmd(linear,0.4;addx,-SCREEN_WIDTH*1.7);
-	};
-	RightToLeft..{
-		InitCommand=cmd(x,SCREEN_RIGHT+256;y,SCREEN_CENTER_Y+48);
-		OnCommand=cmd(linear,0.4;addx,-SCREEN_WIDTH*1.7);
-	};
-	RightToLeft..{
-		InitCommand=cmd(x,SCREEN_RIGHT+320;y,SCREEN_CENTER_Y+112);
-		OnCommand=cmd(linear,0.4;addx,-SCREEN_WIDTH*1.7);
-	};
-	RightToLeft..{
-		InitCommand=cmd(x,SCREEN_RIGHT+384;y,SCREEN_CENTER_Y+176);
-		OnCommand=cmd(linear,0.4;addx,-SCREEN_WIDTH*1.7);
-	};
-};
+	t[#t+1] = RightToLeft..{
+		InitCommand=function(s)
+			s:xy( SCREEN_RIGHT+64*i,SCREEN_CENTER_Y-144+(64*(i-1)) )
+		end,
+		OnCommand=function(s)
+			s:linear(0.4):addx(-SCREEN_WIDTH*1.7)
+		end
+	}
+end
+
+return t;
