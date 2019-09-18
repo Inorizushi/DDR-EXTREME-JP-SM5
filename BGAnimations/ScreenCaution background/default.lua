c0643dbb7bf12a381c99b0cbf5168719506f696e
inorizushi <inorizushi@gmail.com>
2019-09-17 19:57:07 -0500

    Play "Let's DDR" announcer sound on ScreenCaution

--- BGAnimations/ScreenCaution background/default.lua
+++ BGAnimations/ScreenCaution background/default.lua
@@ -2,5 +2,10 @@ return Def.ActorFrame{
 	Def.Sprite{
 		Texture="centered",
 		InitCommand=function(s) s:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y) end
-	}
+	},
+	Def.Actor{
+		OnCommand=function(s)
+			SOUND:PlayAnnouncer("caution");
+		end
+	},
 }
