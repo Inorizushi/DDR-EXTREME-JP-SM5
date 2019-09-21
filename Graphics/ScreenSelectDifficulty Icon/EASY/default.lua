3585d56352c235576e0dc24b278d10b07e42178f
inorizushi <inorizushi@gmail.com>
2019-09-20 19:18:52 -0500

    Continue work on SelectDifficulty, move banner frame, add more fonts for japanese and korean text (they're literally taken from 3.9 default, don't judge me)

--- Graphics/ScreenSelectDifficulty Icon/EASY/default.lua
+++ Graphics/ScreenSelectDifficulty Icon/EASY/default.lua
@@ -3,7 +3,7 @@ return Def.ActorFrame {
 		InitCommand=cmd(draworder,99);
 		OnCommand=cmd(draworder,60;cropbottom,1;sleep,0.264;sleep,0.033;cropleft,0.493;cropright,0.493;linear,0.264;cropbottom,0;linear,0.726;cropleft,0;cropright,0);
 		OffCommand=cmd(cropbottom,0;cropleft,0;cropright,0;sleep,0.726;linear,0.726;cropleft,0.493;cropright,0.493;linear,0.264;cropbottom,1);
-		SwitchToPage1Command=cmd(diffusealpha,1;sleep,0.2;linear,0.5;cropleft,0;cropright,0);
-		SwitchToPage2Command=cmd(sleep,0.2;linear,0.5;cropleft,1;cropright,1;sleep,0;diffusealpha,0);
+		SwitchToPage1Command=cmd(diffusealpha,1;linear,0.4;cropleft,0;cropright,0);
+		SwitchToPage2Command=cmd(linear,0.4;cropleft,1;cropright,1;sleep,0;diffusealpha,0);
 	};
 };
--- Graphics/ScreenSelectDifficulty Icon/ONI/default.lua
+++ Graphics/ScreenSelectDifficulty Icon/ONI/default.lua
@@ -8,7 +8,7 @@ return Def.ActorFrame{
 		InitCommand=cmd(draworder,99;diffusealpha,0);
 		OnCommand=cmd(draworder,60;cropleft,1;cropright,1);
 		OffCommand=cmd(cropbottom,0;cropleft,0;cropright,0;sleep,0.726;linear,0.726;cropleft,0.493;cropright,0.493;linear,0.264;cropbottom,1);
-		SwitchToPage1Command=cmd(sleep,0.2;linear,0.5;cropleft,1;cropright,1;sleep,0;diffusealpha,0);
-		SwitchToPage2Command=cmd(diffusealpha,1;sleep,0.2;linear,0.5;cropleft,0;cropright,0);
+		SwitchToPage1Command=cmd(linear,0.4;cropleft,1;cropright,1;sleep,0;diffusealpha,0);
+		SwitchToPage2Command=cmd(diffusealpha,1;linear,0.4;cropleft,0;cropright,0);
 	};
 };
