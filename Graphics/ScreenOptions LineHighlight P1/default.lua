a9abe4bcfb4c4a584dca9b4deb136f0acbfd2de1
Jose_Varela <josevarela@tutanota.com>
2019-09-22 02:40:39 -0500

    Reactive Highlights

--- Graphics/ScreenOptions LineHighlight P1/default.lua
+++ Graphics/ScreenOptions LineHighlight P1/default.lua
@@ -1,10 +1,28 @@
-local t = ...;
+local t = ...
 t = Def.ActorFrame{
 	Def.Quad{
-		InitCommand=cmd(setsize,SCREEN_WIDTH,38;diffuse,color("#009878"));
+		InitCommand=function(s) s:setsize(SCREEN_WIDTH,38):diffuse( color("#009878") ) end,
+		UpdateFrameMessageCommand=function(s,param)
+			if param.PCont then
+				if param.PCont[PLAYER_1] == param.PCont[PLAYER_2] then
+					s:cropbottom(0.5)
+				else
+					s:cropbottom(0)
+				end
+			end
+		end,
 	};
 	LoadActor("badge")..{
-		InitCommand=cmd(x,(-(SCREEN_WIDTH/2))+35);
+		InitCommand=function(s) s:x( (-(SCREEN_WIDTH/2))+35 ) end,
+		UpdateFrameMessageCommand=function(s,param)
+			if param.PCont then
+				if param.PCont[PLAYER_1] == param.PCont[PLAYER_2] then
+					s:croptop(0.25):y(-10)
+				else
+					s:croptop(0):y(0)
+				end
+			end
+		end,
 	};
 };
 return t;
--- Graphics/ScreenOptions LineHighlight P2/default.lua
+++ Graphics/ScreenOptions LineHighlight P2/default.lua
@@ -2,9 +2,27 @@ local t = ...;
 t = Def.ActorFrame{
 	Def.Quad{
 		InitCommand=cmd(setsize,SCREEN_WIDTH,38;diffuse,color("#b87000"));
+		UpdateFrameMessageCommand=function(s,param)
+			if param.PCont then
+				if param.PCont[PLAYER_1] == param.PCont[PLAYER_2] then
+					s:croptop(0.5)
+				else
+					s:croptop(0)
+				end
+			end
+		end,
 	};
 	LoadActor("badge")..{
-		InitCommand=function(s) s:x((SCREEN_WIDTH/2)):halign(1) end
+		InitCommand=function(s) s:x((SCREEN_WIDTH/2)):halign(1) end,
+		UpdateFrameMessageCommand=function(s,param)
+			if param.PCont then
+				if param.PCont[PLAYER_1] == param.PCont[PLAYER_2] then
+					s:cropbottom(0.25):croptop(0.3):y(8)
+				else
+					s:cropbottom(0):y(0)
+				end
+			end
+		end,
 	};
 };
 return t;
