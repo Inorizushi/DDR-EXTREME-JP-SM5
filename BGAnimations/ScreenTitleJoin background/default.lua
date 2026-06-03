614c058c8acc07144a812739a4aa48806af47ac7
Jose Varela <jose@josevarela.net>
2026-06-03 15:18:08 -0600

    Add MCard available on join
    Also on the table: Fix textures that had some pixels eaten from the magic wand, as I realized how they're filtered via color like the old days.

--- BGAnimations/ScreenTitleJoin background/default.lua
+++ BGAnimations/ScreenTitleJoin background/default.lua
@@ -7,8 +7,34 @@ return Def.ActorFrame{
 		InitCommand=cmd(Center);
 	};]]
 	LoadActor("start")..{
-		InitCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_BOTTOM-70;diffuseblink;linear,0.5;effectcolor1,color(".5,.5,.5,1"));
+		InitCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_BOTTOM-70;diffuseblink;effectperiod,0.3;linear,0.5;effectcolor1,color(".5,.5,.5,1"));
 	};
+
+	Def.ActorFrame{
+		Condition=PREFSMAN:GetPreference("MemoryCards"),
+		InitCommand=function(self)
+			self:Center():y(SCREEN_CENTER_Y-16)
+			:diffuseshift():effectperiod(1.25)
+			:effectcolor1(color("0.5,0.5,0.5,1"))
+		end;
+		LoadActor( THEME:GetPathB("ScreenProfileLoad","overlay/jp_Warning3") )..{
+			InitCommand=function(self)
+				self:x(-80)
+			end;
+		};
+
+		LoadActor("jp_memcapable1")..{
+			InitCommand=function(self)
+				self:x(110)
+			end;
+		};
+
+		LoadActor("jp_memcapable2")..{
+			InitCommand=function(self)
+				self:y(40):diffuse(color("1,1,0.4,1"))
+			end;
+		};
+	}
 	
 	--[[LoadActor(THEME:GetPathG("", "USB icon"))..{
 		InitCommand=cmd(horizalign,right;vertalign,bottom;xy,SCREEN_RIGHT-5,SCREEN_BOTTOM;zoom,.2);
