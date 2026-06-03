6cee66b561072d7c5f4b88aabd69515ea6984929
Jose Varela <jose@josevarela.net>
2026-06-03 13:37:38 -0600

    Add MemoryCard SWME decoration

--- BGAnimations/ScreenTitleJoin background/default.lua
+++ BGAnimations/ScreenTitleJoin background/default.lua
@@ -1,25 +1,3 @@
-function memcardActor(player)
-
-	return LoadActor(THEME:GetPathG("MemoryCard", "Icons"))..{
-		InitCommand=cmd(vertalign,bottom;xy,THEME:GetMetric("ScreenSystemLayer","Credits"..pname(player).."X"),SCREEN_BOTTOM;zoom,2;animate,false;);
-		OnCommand=cmd(visible,ToEnumShortString(MEMCARDMAN:GetCardState(player)) == 'ready');
-		StorageDevicesChangedMessageCommand=function(self)
-			local memCardState = ToEnumShortString(MEMCARDMAN:GetCardState(player))
-			self:visible(true)
-			if memCardState == 'checking' then
-				self:setstate(2);
-			elseif memCardState == "ready" then
-				self:setstate(1);
-			elseif memCardState == "none" then
-				self:visible(false)
-			else
-				self:setstate(4)
-			end;
-		end;
-	};
-end
-
-
 return Def.ActorFrame{
 	Def.Sprite{
 		Texture=THEME:GetPathB("ScreenLogo","background/bg.png");
@@ -31,11 +9,6 @@ return Def.ActorFrame{
 	LoadActor("start")..{
 		InitCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_BOTTOM-70;diffuseblink;linear,0.5;effectcolor1,color(".5,.5,.5,1"));
 	};
-	
-	-- Memory cards
-	memcardActor(PLAYER_1);
-	memcardActor(PLAYER_2);
-
 	
 	--[[LoadActor(THEME:GetPathG("", "USB icon"))..{
 		InitCommand=cmd(horizalign,right;vertalign,bottom;xy,SCREEN_RIGHT-5,SCREEN_BOTTOM;zoom,.2);
---
+++ Graphics/ScreenWithMenuElements MemoryCard/default.lua
@@ -0,0 +1,38 @@
+local t = {};
+
+local memCardEnabled = PREFSMAN:GetPreference("MemoryCards");
+if memCardEnabled then
+	function memcardActor(player)
+		local plrOffset = player == PLAYER_1 and 0 or 5
+		return LoadActor(THEME:GetPathG("MemoryCard", "Icons"))..{
+			OnCommand=function(self)
+				self:visible(ToEnumShortString(MEMCARDMAN:GetCardState(player)) == 'ready')
+				self:x( player == PLAYER_1 and 0 or 24 )
+				self:animate(0)
+				if PROFILEMAN:ProfileWasLoadedFromMemoryCard(player) then
+					self:setstate(plrOffset+1)
+				end
+			end,
+			StorageDevicesChangedMessageCommand=function(self)
+				local memCardState = ToEnumShortString(MEMCARDMAN:GetCardState(player))
+				self:visible(true)
+				if memCardState == 'checking' then
+					self:setstate(plrOffset+2);
+				elseif memCardState == "ready" then
+					self:setstate(plrOffset+1);
+				elseif memCardState == "none" then
+					self:visible(false)
+				else
+					self:setstate(plrOffset+4)
+				end;
+			end;
+		};
+	end
+
+	return Def.ActorFrame{
+		memcardActor(PLAYER_1),
+		memcardActor(PLAYER_2)
+	}
+else
+	return Def.Actor {};
+end
