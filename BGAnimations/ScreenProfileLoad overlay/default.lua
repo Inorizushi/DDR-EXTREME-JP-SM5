b7e44929f146e4fbc8380d1c278e800842f1917b
Jose Varela <jose@josevarela.net>
2026-06-03 12:44:44 -0600

    Better MCard loading anim, transition fixes

--- BGAnimations/ScreenProfileLoad overlay/default.lua
+++ BGAnimations/ScreenProfileLoad overlay/default.lua
@@ -53,13 +53,14 @@ t[#t+1] = Def.ActorFrame{
 t[#t+1] = Def.ActorFrame{
 	InitCommand=function(s) s:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y-10) end,
 	ActionCommand=function(s) s:decelerate(0.110):y(SCREEN_CENTER_Y+80):queuecommand("Bounce") end,
-	CardLoadedCommand=function(s) s:finishtweening():linear(0.110):y(SCREEN_CENTER_Y-10) end,
+	CardLoadedCommand=function(s) s:stoptweening():linear(0.2):zoom(1):linear(0.110):y(SCREEN_CENTER_Y-10) end,
 	BounceCommand=function(s)
+		s:stoptweening()
 		s:decelerate(0.3):zoom(0.7)
-		:bounceend(0.2):zoom(0.9)
+		:accelerate(0.2):zoom(0.9)
 		:decelerate(0.2):zoom(1)
-		:bounceend(0.2):zoom(0.9)
-		:decelerate(0.2):zoom(1)
+		:accelerate(0.2):zoom(0.9)
+		-- :decelerate(0.3):zoom(1)
 		:queuecommand("Bounce")
 	end,
 	Def.Sprite{
@@ -67,10 +68,15 @@ t[#t+1] = Def.ActorFrame{
 		ActionCommand=function(s) s:sleep(0.110):queuecommand("Bounce") end,
 		CardLoadedCommand=function(s) s:stoptweening():decelerate(0.1):zoom(1) end,
 		BounceCommand=function(s)
-			s:bouncebegin(0.3):zoomx(0.9):zoomy(1.1)
-			:smooth(0.15):zoom(1)
-			:bouncebegin(0.15):zoomx(0.95):zoomy(1.05)
-			:smooth(0.15):zoom(1)
+			s:stoptweening()
+			s:spring(.6):zoomx(0.8):zoomy(1.2)
+			:spring(.6):zoomx(1):zoomy(1)
+			-- :spring(0.2):zoomx(1.1):zoomy(0.9)
+			-- :accelerate(0.2):zoomx(1.1):zoomy(0.9)
+			-- :decelerate(0.2):zoomx(1.05):zoomy(1.05)
+			-- :decelerate(0.2):zoomx(1):zoomy(1)
+			-- :bouncebegin(0.15):zoomx(0.95):zoomy(1.05)
+			-- :smooth(0.15):zoom(1)
 			:queuecommand("Bounce")
 			-- :decelerate(0.2):zoom(1)
 			-- :decelerate(0.4):zoom(1)
--- BGAnimations/ScreenProfileSaveSummary overlay/default.lua
+++ BGAnimations/ScreenProfileSaveSummary overlay/default.lua
@@ -4,11 +4,11 @@ function DDRInput(self)
 		local soundbuttons = {"MenuLeft","MenuRight"}
 		if ToEnumShortString(event.type) == "FirstPress" or ToEnumShortString(event.type) == "Repeat" then
 			self:playcommand(event.button):playcommand("Offset")
-			for v in ivalues(soundbuttons) do
-				if event.button == v then
-					SOUND:PlayOnce( THEME:GetPathS("MusicWheel","change") )
-				end
-			end
+			-- for v in ivalues(soundbuttons) do
+			-- 	if event.button == v then
+			-- 		SOUND:PlayOnce( THEME:GetPathS("MusicWheel","change") )
+			-- 	end
+			-- end
 			GAMESTATE:Env()["ToOptions"] = event.DeviceInput.ago > 0.003
 		end
 		if ToEnumShortString(event.type) == "Release" then
@@ -26,7 +26,7 @@ local t = Def.ActorFrame{
 					Timer:stop()
 				end
 				SOUND:PlayAnnouncer("ScreenProfileSaveSummary MemorySave")
-				s:sleep(0.1):queuecommand("Action"):sleep(2):queuecommand("CardSaved")
+				s:sleep(0.1):queuecommand("Action"):sleep(1):queuecommand("CardSaved")
         	else
             	s:queuecommand("CardSaved")
 			end
@@ -66,7 +66,7 @@ t[#t+1] = LoadActor(THEME:GetPathS("","_ScreenProfileSaveSummary music (loop).og
 }
 
 t[#t+1] = Def.Sprite{
-	Texture=lang.."Warning1",
+	Texture=lang.."_Warning1",
 	InitCommand=function(s)
 		s:xy( SCREEN_CENTER_X,SCREEN_CENTER_Y-130 ):diffuse(Color.Blue)
 		s:visible(false)
@@ -81,16 +81,16 @@ t[#t+1] = Def.Sprite{
 
 t[#t+1] = Def.ActorFrame{
 	InitCommand=function(s)
-		s:xy( SCREEN_CENTER_X,SCREEN_CENTER_Y-80 ):diffuse( color("1,1,0.5,1") )
+		s:xy( SCREEN_CENTER_X,SCREEN_CENTER_Y-60 ):diffuse( color("1,1,0.5,1") )
 		s:addx( -SCREEN_WIDTH ):decelerate(0.3):addx( SCREEN_WIDTH )
 	end,
 	CardSavedCommand=function(s)
 		s:visible(false)
 	end,
-    Def.Sprite{ Texture=THEME:GetPathB("","ScreenProfileLoad overlay/"..lang.."Warning3"),
+    Def.Sprite{ Texture=THEME:GetPathB("","ScreenProfileLoad overlay/"..lang.."_Warning3"),
         InitCommand=function(s) s:xy( -140,0 ) end
     };
-    Def.Sprite{ Texture=THEME:GetPathB("","ScreenProfileLoad overlay/"..lang.."Warning4"),
+    Def.Sprite{ Texture=THEME:GetPathB("","ScreenProfileLoad overlay/"..lang.."_Warning4"),
         InitCommand=function(s) s:xy( 120,0 ) end
     };
 }
@@ -106,36 +106,44 @@ t[#t+1] = Def.ActorFrame{
 	CardSavedCommand=function(s)
 		s:visible(true)
 	end,
-    Def.Sprite{ Texture=lang.."Warning3",
+    Def.Sprite{ Texture=lang.."_Warning3",
         InitCommand=function(s) s:xy( -110,0 ) end
     };
-    Def.Sprite{ Texture=lang.."Warning5",
+    Def.Sprite{ Texture=lang.."_Warning5",
         InitCommand=function(s) s:xy( 112,0 ) end
     };
 }
 
 t[#t+1] = Def.ActorFrame{
 	InitCommand=function(s) s:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y-10) end,
-    ActionCommand=function(s) s:decelerate(0.2):y(SCREEN_CENTER_Y+80):queuecommand("Bounce") end,
-    OffCommand=function(s) s:finishtweening():decelerate(0.2):y(SCREEN_CENTER_Y-10):sleep(1) end,
-	CardSavedCommand=function(s) s:finishtweening():y(SCREEN_CENTER_Y+80) end,
+	ActionCommand=function(s) s:decelerate(0.110):y(SCREEN_CENTER_Y+80):queuecommand("Bounce") end,
+	CardSavedCommand=function(s) s:stoptweening():linear(0.2):zoom(1):linear(0.110):y(SCREEN_CENTER_Y+80) end,
+	OffCommand=function(s)
+		s:decelerate(0.2):y(SCREEN_CENTER_Y-10)
+	end,
 	BounceCommand=function(s)
-		s:stoptweening():decelerate(0.3):zoom(0.7)
-		:bounceend(0.2):zoom(0.9)
+		s:stoptweening()
+		s:decelerate(0.3):zoom(0.7)
+		:accelerate(0.2):zoom(0.9)
 		:decelerate(0.2):zoom(1)
-		:bounceend(0.2):zoom(0.9)
-		:decelerate(0.2):zoom(1)
+		:accelerate(0.2):zoom(0.9)
+		-- :decelerate(0.3):zoom(1)
 		:queuecommand("Bounce")
 	end,
 	Def.Sprite{
 		Texture="../_moveon",
-		ActionCommand=function(s) s:sleep(0.2):queuecommand("Bounce") end,
+		ActionCommand=function(s) s:sleep(0.110):queuecommand("Bounce") end,
 		CardSavedCommand=function(s) s:stoptweening():decelerate(0.1):zoom(1) end,
 		BounceCommand=function(s)
-			s:stoptweening():bouncebegin(0.3):zoomx(0.9):zoomy(1.1)
-			:smooth(0.15):zoom(1)
-			:bouncebegin(0.15):zoomx(0.95):zoomy(1.05)
-			:smooth(0.15):zoom(1)
+			s:stoptweening()
+			s:spring(.6):zoomx(0.8):zoomy(1.2)
+			:spring(.6):zoomx(1):zoomy(1)
+			-- :spring(0.2):zoomx(1.1):zoomy(0.9)
+			-- :accelerate(0.2):zoomx(1.1):zoomy(0.9)
+			-- :decelerate(0.2):zoomx(1.05):zoomy(1.05)
+			-- :decelerate(0.2):zoomx(1):zoomy(1)
+			-- :bouncebegin(0.15):zoomx(0.95):zoomy(1.05)
+			-- :smooth(0.15):zoom(1)
 			:queuecommand("Bounce")
 			-- :decelerate(0.2):zoom(1)
 			-- :decelerate(0.4):zoom(1)
