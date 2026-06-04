23a9f304c999acafd8ed0988b0f7004d9a12a3c5
Jose Varela <jose@josevarela.net>
2026-06-04 13:04:25 -0600

    Overhaul wheel fade diffuse
    Now it just uses a single set instead of 11 per wheel item.

--- Graphics/MusicWheelItem Song OverPart/default.lua
+++ Graphics/MusicWheelItem Song OverPart/default.lua
@@ -1,17 +1,16 @@
 local t = Def.ActorFrame{}
 
-local alphas = {0.5 ,0.5, 0.4, 0.3, 0.2, 0, 0.2, 0.3, 0.4, 0.5, 0.5};
-local indexes = {0,1, 2, 3, 4, 5, 6, 7, 8, 9,10};
+local alphas = {0.5 ,0.5, 0.4, 0.3, 0.2, 0, 0.2, 0.3, 0.4, 0.5, 0.5, 0.5, 0.5};
+local indexes = {0,1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12};
 
-for i = 1,11 do
 t[#t+1] = Def.ActorFrame{
   LoadActor("../_witem over")..{
     SetMessageCommand=function(self,params)
       local index = params.DrawIndex
       if index then
-        if index == indexes[i] then
+        if indexes[index] then
           self:diffuse(Color.Black)
-          self:diffusealpha(alphas[i])
+          self:diffusealpha(alphas[index] or 0.5)
         else
           self:diffusealpha(0)
         end;
@@ -22,6 +21,5 @@ t[#t+1] = Def.ActorFrame{
     end;
   };
 };
-end;
 
 return t;
